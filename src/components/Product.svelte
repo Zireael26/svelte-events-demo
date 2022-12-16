f32403d7c334eecb535a2e57ae49a7a55fca6ea4
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2022-12-17 02:54:55 +0530

    Added example for custom events

---
+++ src/components/Product.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	export let id: string;
+	export let title: string;
+	export let price: number;
+	export let bestseller = false;
+
+	const dispatch = createEventDispatcher();
+
+	function addToCart() {
+		dispatch('add-to-cart', { id: id });
+	}
+</script>
+
+<article>
+	<h1>{title}</h1>
+	<h2>${price}</h2>
+	{#if bestseller}
+		<h3>Bestseller!</h3>
+	{/if}
+	<button on:click={addToCart}>Add to Cart</button>
+	<button
+		on:click={() => {
+			dispatch('delete-product', id);
+		}}>Delete</button
+	>
+</article>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Product from '../components/Product.svelte';
+
+	let products = [
+		{ id: 'p1', title: 'A Book', price: 10.99 },
+		{ id: 'p2', title: 'A Carpet', price: 89.99 }
+	];
+
+	function addToCart(event: CustomEvent) {
+		console.log('Add to cart: ', event);
+	}
+
+	function deleteProduct(event: CustomEvent) {
+		console.log('Delete Product', event);
+	}
+</script>
+
+{#each products as product (product.id)}
+	<Product {...product} on:add-to-cart={addToCart} on:delete-product={deleteProduct} />
+{/each}
